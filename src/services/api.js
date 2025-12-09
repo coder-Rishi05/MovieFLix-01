eec055eb0d64c7a048a085e21bdbf822fd11d3fa
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-12-09 20:05:42 +0530

    added api key

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = "01b9857b6c02ce28cd7c5e8c079d0444";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
