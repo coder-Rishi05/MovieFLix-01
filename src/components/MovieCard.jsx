71aef54eee6ec0fd047e960397cb1d4ba5a9477c
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-12-13 15:55:35 +0530

    added movie context

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/MovieCard.css";
+import { Heart } from "lucide-react";
 
 const MovieCard = ({ movie }) => {
   function favMovie() {
@@ -7,18 +8,18 @@ const MovieCard = ({ movie }) => {
   }
 
   return (
-    <div className="movie-card">
-      <div className="movie-poster">
+    <div className="movie-card ">
+      <div className="movie-poster cursor-pointer relative">
         <img
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
         />
-        <div className="movie-overlay">
+        <div className="movie-overlay ">
           <button className="btn" onClick={favMovie}>
-            💖
+           <Heart className="text-white cursor-pointer top-6 right-4 absolute " />
           </button>
         </div>
-      </div>
+      </div> 
       <div className="movie-info">
         <h2 className="text-xl text-amber-50" >{movie.title}</h2>
         <p>{movie.release_date?.split("-")[0]}</p>
